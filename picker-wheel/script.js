0504f102225d47b0e89e0d0d792ff7f63fc1e988
Nicklas Kenyon <nick@kinoteksoftware.com>
2023-04-27 22:10:10 -0400

    add emotes to chat, wip picker wheel

--- picker-wheel/script.js
+++ picker-wheel/script.js
@@ -13,19 +13,20 @@ function drawWheel(angleOffset) {
   ctx.beginPath();
   ctx.moveTo(centerX, centerY);
   ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
-  ctx.closePath();
   ctx.fillStyle = "white";
   ctx.fill();
+  ctx.closePath();
   for (let i = 0; i < options.length; i++) {
+    let region = new Path2D();
     const option = i + 1;
-    ctx.beginPath();
-    ctx.moveTo(centerX, centerY);
+    // region.beginPath();
+    region.moveTo(centerX, centerY);
     const optionAngle = option * wedgeAngle + angleOffset;
-    ctx.arc(centerX, centerY, radius, optionAngle, optionAngle + wedgeAngle);
-    ctx.lineTo(centerX, centerY);
-    ctx.closePath();
-    ctx.fillStyle = i % 2 === 0 ? "#E6E6E6" : "#F5F5F5";
-    ctx.fill();
+    region.arc(centerX, centerY, radius, optionAngle, optionAngle + wedgeAngle);
+    region.lineTo(centerX, centerY);
+    region.closePath();
+    ctx.fillStyle = i % 2 === 0 ? "cyan" : "magenta";
+    ctx.fill(region, "evenodd");
     ctx.font = "20px Arial";
     ctx.fillStyle = "black";
     ctx.translate(centerX, centerY);
@@ -38,29 +39,32 @@ function drawWheel(angleOffset) {
 }
 
 function spin() {
-  const spins = 5 + Math.floor(Math.random() * 8);
-  const angle = (2 * Math.PI) / options.length;
-  const speed = 0.125;
+  const fps = 30;
+  const frameLength = 1000 / fps;
+  const spins = 5 + Math.floor(Math.random() * 6);
+  const result = Math.floor(Math.random() * options.length);
+  const angle = ((2 * Math.PI) / options.length * result) + (2 * Math.PI);
+  const speed = (Math.PI / 2) / fps;
   let rotation = 0;
   let spinCount = 0;
-  let result = null;
   const animate = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.save();
     drawWheel(rotation);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(centerX + radius, centerY - 6, 32, 12);
     ctx.restore();
-    rotation += speed * Math.PI;
+    rotation += speed;
     if (rotation >= angle) {
       spinCount++;
       if (spinCount === spins) {
         clearInterval(animId);
-        result = options[Math.floor((options.length * rotation) / (2 * Math.PI))];
-        alert("You landed on: " + result);
+        alert("You landed on: " + options[result]);
       }
     }
   };
   animate();
-  const animId = setInterval(animate, 1000 / 30);
+  const animId = setInterval(animate, frameLength);
 }
 
 spin();
